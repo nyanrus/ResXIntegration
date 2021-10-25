d85c6184f12b68f399b13b6d3f177cba5d3f864c
touyu7720 <touyu1829@gmail.com>
2021-10-25 19:20:41 +0900

    third

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using System.Resources;
+using System.Linq;
 //using System.Windows.Forms;
 
 namespace ResXFoo
@@ -11,51 +12,47 @@ namespace ResXFoo
     {
         static void Main()
         {
-            List<ResXResourceWriter> rsxw = new List<ResXResourceWriter>();
+            var rsxw = new List<ResXResourceWriter>();
             try
             {
-                List<string> fnames = new List<string>();
-                string[] names = Directory.GetFiles(@"./resxFolder","*.resx",SearchOption.AllDirectories);
-                List<string> namee = new List<string>();
-                
+                //HashSet = 重複不可
+                var HSfileName = new HashSet<string>();
+
+                //resxファイル取得,ファイル名で整列
+                var names = Directory.GetFiles(@"./resxFolder","*.resx",SearchOption.AllDirectories)
+                .OrderBy(f => Path.GetFileName(f))
+                .ToList<string>();
+
                 //重複してないResXファイル名リスト作成
                 foreach(string name in names)
                 {
-                    if (fnames.Contains(Path.GetFileName(name)) == false)
-                    {
-                        fnames.Add(Path.GetFileName(name));
-
-                        //ResX書き込みオブジェクト生成
-                        rsxw.Add(new ResXResourceWriter(Path.GetFileName(name)));
-                    }
+                    HSfileName.Add(Path.GetFileName(name));
                 }
 
-                //ファイル名でファイルパス整列
-                for(int i = 0; i < fnames.Count; ++i)
+                foreach(var name in HSfileName)
                 {
-                    for(int j = 0; j < names.Length; ++j)
-                    {
-                        if (fnames[i] == Path.GetFileName(names[i]))
-                        {
-                            namee.Add(names[j]);
-                        }
-                    }
+                    //ResX書き込みオブジェクト生成
+                    rsxw.Add(new ResXResourceWriter(name));
                 }
+
+                var fnames = HSfileName.ToList<string>();
+
                 var errKey = new List<List<string>>();
                 var errVal = new List<List<string>>();
 
+                //ファイル名分リスト作成
                 for(int i = 0; i < fnames.Count; ++i)
                 {
                     errKey.Add(new List<string>());
                     errVal.Add(new List<string>());
                 }
-                
+
                 int tmp = 0;
                 int tmpa = 0;
                 int tmpb = 0;
                 bool cont = false;
                 //ファイルパス
-                foreach (string name in namee)
+                foreach (string name in names)
                 {
                     int a = 0;
                     int all = 0;
@@ -78,8 +75,9 @@ namespace ResXFoo
                                             if (errVal[i][j] != entry.Value.ToString())
                                             {
                                                 ++tmpb;
+                                                Console.WriteLine("Name : " + name);
+                                                Console.WriteLine("Error : " + entry.Key.ToString());
                                             }
-                                            Console.WriteLine("Error : " + entry.Key.ToString());
                                             cont = true;
                                             break;
                                         }
@@ -89,22 +87,22 @@ namespace ResXFoo
                                         cont = false;
                                         continue;
                                     }
-                                    
                                     errKey[i].Add(entry.Key.ToString());
                                     errVal[i].Add(entry.Value.ToString());
                                     rsxw[i].AddResource(entry.Key.ToString(),entry.Value.ToString());
                                     ++all;
                                 }
                             }
+                            /*
                             Console.WriteLine("filename : " + name);
                             Console.WriteLine("All num : " + all);
                             Console.WriteLine("error num : " + a);
+                            */
                             tmp += all;
                             tmpa += a;
 
                         }
                     }
-                    
                 }
                 Console.WriteLine("----------");
                 Console.WriteLine("All num : "+ tmp);
@@ -122,6 +120,50 @@ namespace ResXFoo
                     //ResX書き込みオブジェクト開放
                     resw.Dispose();
                 }
+            }
+
+            var writer = new List<StreamWriter>();
+            try
+            {
+                var names = Directory.GetFiles(@"./resxFolder","*.Designer.cs",SearchOption.AllDirectories)
+                .OrderBy(f => Path.GetFileName(f))
+                .ToList<string>();
+
+                var HSfileName = new HashSet<string>();
+
+                foreach(string name in names)
+                {
+                    HSfileName.Add(Path.GetFileName(name));
+                }
+
+                foreach(var name in HSfileName)
+                {
+                    writer.Add(new StreamWriter(name));
+                }
+
+                var fileName = HSfileName.ToList<string>();
+
+
+                for(int i = 0; i < fileName.Count; i++)
+                {
+                    foreach(var name in names)
+                    {
+                        if (Path.GetFileName(name) == fileName[i])
+                        {
+                            using(StreamReader reader = new StreamReader(name))
+                            {
+                                while(!reader.EndOfStream)
+                                {
+                                    writer[i].WriteLine(reader.ReadLine());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
     }
